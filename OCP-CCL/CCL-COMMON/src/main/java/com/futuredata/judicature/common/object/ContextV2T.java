46db70e2b53da95df924f2657e1a173d87a306d9
yu.yao <1056146816@qq.com>
2017-12-12 03:40:58 +0800

    common工程修改

---
+++ OCP-CCL/CCL-COMMON/src/main/java/com/futuredata/judicature/common/object/ContextV2T.java
@@ -0,0 +1,9 @@
+package com.futuredata.judicature.common.object;
+
+/**
+ * VO与DTO交互层的应用上下文
+ * 
+ * @author yu.yao
+ */
+public class ContextV2T {
+}
---
+++ OCP-CCL/CCL-COMMON/src/main/java/com/futuredata/judicature/common/object/transfer/AbstractDTO.java
@@ -0,0 +1,10 @@
+package com.futuredata.judicature.common.object.transfer;
+
+/**
+ * DTO抽象模板
+ * 
+ * @author yu.yao
+ */
+public abstract class AbstractDTO {
+  public abstract void A();
+}
