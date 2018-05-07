f56084a21b3a6d867981e09129c4341972ae5e22
yu.yao <yu.yao@future-data.cn>
2018-05-07 18:39:27 +0800

    jni

--- OCP-AIE/AIE-VISION/ocr-jni/src/main/java/com/ocp/ai/engine/vision/ocr/OCRJNI.java
+++ OCP-AIE/AIE-VISION/ocr-jni/src/main/java/com/ocp/ai/engine/vision/ocr/OCRJNI.java
@@ -1,24 +1,18 @@
 package com.ocp.ai.engine.vision.ocr;
 
 public class OCRJNI {
+
   static {
-    System.load("E:/path/tesseract40.dll");
-    System.load("E:/path/libdll.dll");
+    System.load("/usr/local/lib/libtesseract.so");
   }
 
   /**
    * 从一张图片中识别文字
    * 
    * @author Alex
-   * @param path 图片绝对路径
-   * @param type 图片文件类型
-   * @param simpleName 图片文件简称(不包含后缀)
+   * @param input 输入
+   * @param output 输出
    * @return
    */
-  public native String convert(String path, String type, String simpleName);
-
-  public static void main(String[] args) {
-    OCRJNI jni_ins = new OCRJNI();
-    jni_ins.convert("/src/resource/6.tif", "tif", "6");
-  }
+  public native String convert(String input, String output);
 }
